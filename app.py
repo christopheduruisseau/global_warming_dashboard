7543474e7c140d7f9cb162d5a448325f617f5090
Chris <christophe.duruisseau@gmail.com>
2020-10-31 18:11:51 +0100

    first draft with title, one row (map and txtarea) and sidebar with links, also add gitignore file

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-
+import pandas as pd
+import numpy as np
 import plotly.express as px
 
 # the style arguments for the sidebar.
@@ -12,20 +13,20 @@ SIDEBAR_STYLE = {
     'top': 0,
     'left': 0,
     'bottom': 0,
-    'width': '20%',
+    'width': '15%',
     'padding': '20px 10px',
     'background-color': '#f8f9fa'
 }
 
 # the style arguments for the main content page.
 CONTENT_STYLE = {
-    'margin-left': '25%',
+    'margin-left': '18%',
     'margin-right': '5%',
-    'padding': '20px 10p'
+    'padding' : '10px 5px 15px 20px' 
 }
 
 TEXT_STYLE = {
-    'textAlign': 'center',
+    'textAlign': 'left',
     'color': '#191970'
 }
 
@@ -34,205 +35,104 @@ CARD_TEXT_STYLE = {
     'color': '#0074D9'
 }
 
+df = pd.read_csv('co-emissions-per-capita.csv')
+df_country = df[~df['Code'].isnull()]
+df_country.reset_index(drop=True,inplace=True)
+df['Year'] = pd.to_datetime(df['Year'],format='%Y')
+fig = px.choropleth(df_country, locations="Code", color="Per capita CO2 emissions", color_continuous_scale='Inferno',range_color=[0,30], animation_frame="Year")
+fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+fig.layout.width=800
+fig.layout.height=500
+
+fig.layout.modebar.orientation='h'
+
+fig.layout.coloraxis.colorbar.x = -0.1
+fig.layout.coloraxis.colorbar.xanchor = 'left'
+
+fig.layout.updatemenus[0]['pad']={'t':-0.3}
+fig.layout.updatemenus[0].direction = 'up'
+fig.layout.updatemenus[0].x = -0.1
+fig.layout.updatemenus[0].xanchor = 'left'
+
+fig.layout.sliders[0]['pad']={'t':-0.3}
+fig.layout.sliders[0]['len']= 1
+fig.layout.sliders[0].x = 0
+
 controls = dbc.FormGroup(
     [
-        html.P('Dropdown', style={
+        html.A('Worldwide temperatures',href='#card_title_1', style={
             'textAlign': 'center'
         }),
-        dcc.Dropdown(
-            id='dropdown',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            }, {
-                'label': 'Value Two',
-                'value': 'value2'
-            },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value=['value1'],  # default value
-            multi=True
-        ),
         html.Br(),
-        html.P('Range Slider', style={
+       html.A('Worldwide greenhouse gas emissions ',href='#card_title_1', style={
             'textAlign': 'center'
         }),
-        dcc.RangeSlider(
-            id='range_slider',
-            min=0,
-            max=20,
-            step=0.5,
-            value=[5, 15]
-        ),
-        html.P('Check Box', style={
-            'textAlign': 'center'
-        }),
-        dbc.Card([dbc.Checklist(
-            id='check_list',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            },
-                {
-                    'label': 'Value Two',
-                    'value': 'value2'
-                },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value=['value1', 'value2'],
-            inline=True
-        )]),
         html.Br(),
-        html.P('Radio Items', style={
+       html.A('Effect of global warming',href='#card_title_1', style={
             'textAlign': 'center'
         }),
-        dbc.Card([dbc.RadioItems(
-            id='radio_items',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            },
-                {
-                    'label': 'Value Two',
-                    'value': 'value2'
-                },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value='value1',
-            style={
-                'margin': 'auto'
-            }
-        )]),
-        html.Br(),
-        dbc.Button(
-            id='submit_button',
-            n_clicks=0,
-            children='Submit',
-            color='primary',
-            block=True
-        ),
+         html.Br(),
+       html.A('Predictions',href='#card_title_1', style={
+            'textAlign': 'center'
+        }),      
     ]
 )
 
-sidebar = html.Div(
+sidebar = html.Div( 
     [
-        html.H2('Parameters', style=TEXT_STYLE),
+        html.H2('Sommaire', style=TEXT_STYLE),
         html.Hr(),
         controls
     ],
     style=SIDEBAR_STYLE,
 )
 
-content_first_row = dbc.Row([
-    dbc.Col(
-        dbc.Card(
-            [
+content_zero_row = dbc.Row(
+    [
+        html.H4('Worldwide temperature',style=TEXT_STYLE),
+    ]
+)
 
-                dbc.CardBody(
-                    [
-                        html.H4(id='card_title_1', children=['Card Title 1'], className='card-title',
-                                style=CARD_TEXT_STYLE),
-                        html.P(id='card_text_1', children=['Sample text.'], style=CARD_TEXT_STYLE),
-                    ]
-                )
-            ]
-        ),
-        md=3
-    ),
-    dbc.Col(
-        dbc.Card(
-            [
-
-                dbc.CardBody(
-                    [
-                        html.H4('Card Title 2', className='card-title', style=CARD_TEXT_STYLE),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
-                    ]
-                ),
-            ]
-
-        ),
-        md=3
-    ),
-    dbc.Col(
-        dbc.Card(
-            [
-                dbc.CardBody(
-                    [
-                        html.H4('Card Title 3', className='card-title', style=CARD_TEXT_STYLE),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
-                    ]
-                ),
-            ]
-
-        ),
-        md=3
-    ),
-    dbc.Col(
-        dbc.Card(
-            [
-                dbc.CardBody(
-                    [
-                        html.H4('Card Title 4', className='card-title', style=CARD_TEXT_STYLE),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
-                    ]
-                ),
-            ]
-        ),
-        md=3
-    )
-])
+content_first_row = dbc.Row(align="right", children=
+    [
+        dbc.Col(align='left',children=[
+            dcc.Graph(id='graph_2',
+            config = {
+            'modeBarButtonsToAdd' : ['drawline', 'drawopenpath', 'drawrect', 'eraseshape'],
+            'displayModeBar'         : True
+                    },
+            figure = fig
+            )],md=7),
+        dbc.Col(
+            dbc.Textarea(
+            id='textarea-example',
+            value='Textarea content initialized\nwith multiple lines of text',
+            style={ 'width': '100%', 'min-height': '400px',
+                'box-sizing': 'border-box', 'border': 'none'},
+            ),md=5,
+        ),  
+    ]
+)
 
 content_second_row = dbc.Row(
     [
-        dbc.Col(
-            dcc.Graph(id='graph_1'), md=4
-        ),
-        dbc.Col(
-            dcc.Graph(id='graph_2'), md=4
-        ),
-        dbc.Col(
-            dcc.Graph(id='graph_3'), md=4
-        )
     ]
 )
 
 content_third_row = dbc.Row(
     [
-        dbc.Col(
-            dcc.Graph(id='graph_4'), md=12,
-        )
-    ]
-)
-
-content_fourth_row = dbc.Row(
-    [
-        dbc.Col(
-            dcc.Graph(id='graph_5'), md=6
-        ),
-        dbc.Col(
-            dcc.Graph(id='graph_6'), md=6
-        )
     ]
 )
 
 content = html.Div(
     [
-        html.H2('Analytics Dashboard Template', style=TEXT_STYLE),
+        html.H3('A data centered view on Global Warming', style=TEXT_STYLE),
+        "by Valérie Didier, Esperence Moussa, Colin Verhille, Christophe Duruisseau",
         html.Hr(),
+        content_zero_row,
         content_first_row,
         content_second_row,
         content_third_row,
-        content_fourth_row
     ],
     style=CONTENT_STYLE
 )
@@ -274,13 +174,7 @@ def update_graph_2(n_clicks, dropdown_value, range_slider_value, check_list_valu
     print(range_slider_value)
     print(check_list_value)
     print(radio_items_value)
-    fig = {
-        'data': [{
-            'x': [1, 2, 3],
-            'y': [3, 4, 5],
-            'type': 'bar'
-        }]
-    }
+    fig = px.choropleth(df_country, locations="Code", color="Per capita CO2 emissions", color_continuous_scale='Inferno',range_color=[0,30], animation_frame="Year",title='Per capita CO2 emissions')
     return fig
 
 
@@ -354,36 +248,6 @@ def update_graph_6(n_clicks, dropdown_value, range_slider_value, check_list_valu
     df = px.data.tips()
     fig = px.bar(df, x='total_bill', y='day', orientation='h')
     return fig
-
-
-@app.callback(
-    Output('card_title_1', 'children'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_card_title_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
-    return 'Card Tile 1 change by call back'
-
-
-@app.callback(
-    Output('card_text_1', 'children'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_card_text_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
-    return 'Card text change by call back'
 
 
 if __name__ == '__main__':
